044accccdde4fe0d8543c55a3a0207797cbe0b7f
ravenexp <ravenexp@users.noreply.github.com>
2022-08-21 11:18:30 +0000

    Upload documentation for main

---
+++ main/doc/implementors/core/fmt/trait.Pointer.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"pyo3_ffi":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Pointer.html\" title=\"trait core::fmt::Pointer\">Pointer</a> for <a class=\"union\" href=\"pyo3_ffi/union.PyMethodDefPointer.html\" title=\"union pyo3_ffi::PyMethodDefPointer\">PyMethodDefPointer</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
